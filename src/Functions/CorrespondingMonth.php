a82ec3a7ac9a4a782d7ca29c1c0e865c63e9a391
CaiqueOrtega <dev.caique.ortega@outlook.com>
2025-05-10 22:21:55 -0300

    feat: implement correspondingMonth with tests Closes #1

---
+++ src/Functions/CorrespondingMonth.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Caique\PhpCodingChallenges\Functions;
+
+/**
+ * The function should receive an integer between 1 and 12 (inclusive) and return the corresponding string value. If the informed integger is not between 1 and 12, the function should return 'Unknown Month'
+ * Ex: input: 1 - output: "January"
+ * Ex: input: 13 - output: "Unknown Month"
+ *
+ * A função recebe um inteiro entre 1 e 12 e retorna o mês correspondente por extenso. Caso o mês informado não esteja entre 1 e 12, deverá ser retornado "Mes Inexistente"
+ * Ex: input: 1 	- output: "Janeiro"
+ * Ex: input: 13 	- output: "Mês Desconhecido"
+ *
+ * @param int $month
+ * @return string
+ */
+class CorrespondingMonth
+{
+    public static function get(int $month): string
+    {
+        $months = [
+            1 => 'January', 2 => 'February', 3 => 'March', 4 => 'April',
+            5 => 'May', 6 => 'June', 7 => 'July', 8 => 'August',
+            9 => 'September', 10 => 'October', 11 => 'November', 12 => 'December'
+        ];
+
+        return $months[$month] ?? 'Mês Desconhecido';
+    }
+}
